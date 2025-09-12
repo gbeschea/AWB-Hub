909af704db80d35958a1a02a6d08b415336db40b
gbeschea <gheorghe.beschea@overheat.agency>
2025-09-12 16:37:30 +0300

    d

--- services/couriers/__init__.py
+++ services/couriers/__init__.py
@@ -1,36 +1,22 @@
-# services/couriers/__init__.py
+# gbeschea/awb-hub/AWB-Hub-2de5efa965cc539c6da369d4ca8f3d17a4613f7f/services/couriers/__init__.py
+
 from typing import Optional
-import httpx
 from .base import BaseCourier
-from .dpd import DPDCourier
 from .sameday import SamedayCourier
+from .dpd import DPDCourier
 from .econt import EcontCourier
 
-# Un singur client HTTP partajat pentru toată aplicația, pentru performanță
-_http_client = httpx.AsyncClient(timeout=45.0)
-
-# --- MODIFICARE: Creăm instanțe unice (singletons) pentru fiecare curier ---
-_courier_instances = {
-    "dpd": DPDCourier(_http_client),
-    "sameday": SamedayCourier(_http_client),
-    "econt": EcontCourier(_http_client),
+COURIER_MAP = {
+    "sameday": SamedayCourier,
+    "dpd": DPDCourier,
+    "econt": EcontCourier,
 }
-# --- SFÂRȘIT MODIFICARE ---
 
-
-def get_courier_service(courier_key: str) -> Optional[BaseCourier]:
-    """
-    Funcția Fabrică (Factory Function).
-    Acum returnează o instanță partajată, nu una nouă.
-    """
-    if not courier_key:
-        return None
-        
-    # Caută o potrivire parțială (ex: 'dpd-ro' se va potrivi cu 'dpd')
-    # --- MODIFICARE: Căutăm și returnăm instanța existentă ---
-    for key, instance in _courier_instances.items():
-        if key in courier_key.lower():
-            return instance
-    # --- SFÂRȘIT MODIFICARE ---
-    
+# MODIFICAT: Am adăugat 'account_key: str' la semnătura funcției
+def get_courier_service(courier_name: str, account_key: str) -> Optional[BaseCourier]:
+    """Factory function to get a courier service instance."""
+    courier_class = COURIER_MAP.get(courier_name.lower())
+    if courier_class:
+        # Pasăm 'account_key' la inițializarea clasei
+        return courier_class(account_key=account_key)
     return None
